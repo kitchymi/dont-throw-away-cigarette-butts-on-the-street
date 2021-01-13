16ff22cc020092ef98243a8f3f35b15bc0a70807
KHLee <vindy@gambridzy.com>
2021-01-13 15:33:29 +0900

    test

--- source/main.js
+++ source/main.js
@@ -54,7 +54,7 @@ function init()
 			var touches = event.changedTouches;
 			inputX = touches[0].screenX;
 			inputY = touches[0].screenY;
-			document.getElementById("show").innerText = inputX + ", " + inputY;
+			document.getElementById("show").innerText = inputX + ", " + inputY + "\n" + canvas.width + ", " + canvas.height + "\n" + canvasScale;
 		}, false);
 	canvas.addEventListener("touchend", function(event)
 		{
