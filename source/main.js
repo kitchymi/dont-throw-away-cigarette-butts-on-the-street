9fdb2cf7fab9d62fc9e4dcf2a5b3e61732db4416
KHLee <vindy@gambridzy.com>
2021-01-15 13:27:56 +0900

    0.8

--- source/main.js
+++ source/main.js
@@ -115,6 +115,8 @@ function initializeCanvas(screenWidth, screenHeight)
 	//	"width=" + screenWidth + ", user-scalable=no");
 	//$("#viewport").attr("content",
 	//	"width=" + screenWidth + ", user-scalable=no");
+	$('meta[name=viewport]').attr("content",
+		"width=" + screenWidth + ", user-scalable=no");
 
 	if (aspectRatio > baseRatio)
 	{
